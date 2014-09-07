470ce566711fef8d6366d01681dd12abb2894ee6
danigrant <thedanigrant@gmail.com>
2014-09-06 22:14:44 -0400

    working key exchange

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $('document').ready(function() {
-		
 	// ** This styling still does not work perfectly
 	// Create class for position of button so it is
 	// applied at the correct time??
@@ -34,11 +33,11 @@ $('document').ready(function() {
 	$("#u_0_s2").click( function() {
 		console.log("clicked!");
 		
-		// Change text in DOM to be encrypted
-		encryptSendText( function() {
-			// Call original Reply button's event listener (on callback)
-			$("#u_0_s").trigger("click");
-		});		
+	// Change text in DOM to be encrypted
+	encryptSendText( function() {
+		// Call original Reply button's event listener (on callback)
+		$("#u_0_s").trigger("click");
+	});		
 	});
 
 	function encryptSendText(callback) {
@@ -53,31 +52,19 @@ $('document').ready(function() {
 	}
 	
 	function generateKeys(callback) {
-		console.log("genkeys");
-		console.log(openpgp);
 		keyData = openpgp.generateKeyPair(options);
-		console.log(openpgp);
-		console.log(keyData.publicKeyArmored);
 		var publicKeyString = keyData.publicKeyArmored;
-		console.log(openpgp);
 		var privateKeyString = keyData.privateKeyArmored;
 		callback();
 	}
 
 	function encrypt(str) {
-		console.log("encrypt");
-		console.log(openpgp);
-		console.log(keyData);
-		console.log(keyData.publicKeyArmored);
 		var publicKey = openpgp.key.readArmored(keyData.publicKeyArmored);
 		var pgpMessage = openpgp.encryptMessage(publicKey.keys, str);
-		console.log(pgpMessage);
 		return pgpMessage;
 	}
 
 	function decrypt(pgpMessage) {
-		console.log("decrypt");
-		console.log(openpgp);
 		var privateKey = openpgp.key.readArmored(keyData.privateKeyArmored).keys[0];
 		privateKey.decrypt('pass');
 		pgpMessage = openpgp.message.readArmored(pgpMessage);
@@ -88,13 +75,50 @@ $('document').ready(function() {
 	function findTypedMessage () {
 		var t = document.querySelector("[name=message_body]");
 		var str = t.value;
+		console.log("this is the string");
 		console.log(str);
 		return str;
 	}
 
-	generateKeys( function() {
-		//console.log(decrypt(encrypt('Hello World')));
+	function getLastMessage () {
+        console.log("get last message");
+        var t = $('.webMessengerMessageGroup .clearfix p:nth-last-child(2)').text();
+        return t;
+    }
+
+    function checkForDecrypt () {
+    	console.log("checking for decryption");
+		console.log(getLastMessage().substring());
+    	if(getLastMessage().substring(0, 26) === "-----BEGIN PGP MESSAGE-----") {
+    		console.log("this should be decrypted");
+    	}
+    }
+
+    var remotePublicKey;
+	generateKeys(function() {
+		function checkForEncrypt (text) {
+
+			if (!remotePublicKey) {
+				var isPgpBlock = /^-----BEGIN PGP PUBLIC KEY BLOCK-----/mi.test(text);
+				if (isPgpBlock) {
+					if (text !== publicKey)
+					remotePublicKey = text;
+				}
+				return;
+			} 
+
+			var clearText = decrypt(text);
+			console.log(clearText);
+	    }
+
+		var numberOfMessages = $('._38').length;
+		setInterval(function () { 
+			var newNumberOfMessages = $('._38').length;
+			if (newNumberOfMessages !== numberOfMessages) {
+				var text = $($('._38')[newNumberOfMessages - 1]).text();
+				checkForEncrypt(text);
+				numberOfMessages = newNumberOfMessages;
+			}
+		}, 100);
 	});
-
-
 });
